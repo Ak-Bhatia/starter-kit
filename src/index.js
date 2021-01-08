fdbccf13b2dc8ff3a8e6b466ac83974b8b7d0f5d
akshit <akshitb124210@gmail.com>
2021-01-09 00:16:12 +0530

    check for travis CI(pass)

--- src/index.js
+++ src/index.js
@@ -4,4 +4,4 @@ import numeral from 'numeral';
 
 const courseValue = numeral(1000).format('$0,0.00');
 debugger; // eslint-disable-line no-debugger
-console.log(`I would pay ${courseValue} fro this course!!`); // eslint-disable-line no-console
+console.log(`I would pay ${courseValue} for this course!!`); // eslint-disable-line no-console
